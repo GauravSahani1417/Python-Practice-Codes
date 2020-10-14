9be366dc25051cfe756eb4f8d7d09c0da6b2c39a
Gaurav Rajesh Sahani <gauravsahani1417@gmail.com>
2020-10-14 14:33:52 +0530

    Add files via upload

---
+++ Array Programs/Addition of array.py
@@ -0,0 +1,22 @@
+def _sum(arr, n):
+    return(sum(arr))
+
+arr = []
+arr = [14, 4, 27, 3]
+
+n = sum(arr)
+
+ans = _sum(arr,n)
+
+print ('Sum of the array is ', ans)
+
+#Method No. 2:
+
+arr = [] 
+# input values to list 
+arr = [12, 3, 4, 15] 
+
+ans = sum(arr) 
+  
+# display sum 
+print ('Sum of the array is ',ans)
